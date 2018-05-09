d9a230ce192937def54cff0d524665dea43ef791
Franco Alvarez <wfalvarez1@gmail.com>
2018-05-08 21:03:59 -0400

    organizacion horario

--- src/views/Text.vue
+++ src/views/Text.vue
@@ -43,16 +43,22 @@
         </v-layout>
       </v-flex>
       <v-flex xs12>
-        <v-layout v-if="!loading" row wrap>
-          <v-flex xs6 v-for="(list, index) in lists" :key="index">
-            <v-list>
+        <v-progress-linear v-if="loading" :indeterminate="true"></v-progress-linear>
+      </v-flex>
+    </v-layout>
+    </v-container>
+    <v-layout v-if="!loading && !!materias.length"  row>
+      <!-- <v-flex xs3 class="scroll-box"> -->
+        <!-- <v-layout v-if="!loading" row wrap class="pr-3">
+          <v-flex xs12 v-for="(list, index) in lists" :key="index">
+            <v-list two-line>
               <template v-for="(materia, index) in list">
                 <drag :transfer-data="materia" :key="index">
                   <template slot-scope="props">
                     <v-card dark :color="'color'+ (materia.id_materia+1)">
                       <v-list-tile avatar :key="materia.id" @click="">
                         <v-list-tile-avatar>
-                          <i :class="materia.icon"></i>
+                          <i :class="icons[materia.id_materia]"></i>
                         </v-list-tile-avatar>
                         <v-list-tile-content>
                           <v-list-tile-title>
@@ -78,71 +84,114 @@
               </template>
             </v-list>
           </v-flex>
-        </v-layout>
-        <v-progress-linear v-else :indeterminate="true"></v-progress-linear>
-      </v-flex>
-    </v-layout>
-    </v-container>
-    <v-layout v-if="!loading && !!materias.length"  row>
-      <v-flex xs1>
-        <v-layout row wrap>
-         <v-flex xs12>
-            <v-card tile flat>
-              <v-card-text class="text-xs-center">HORA</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-          <v-flex xs12>
-            <v-card>
-              <v-list class="pt-0 pb-0">
-                <template v-for="(item, index) in hora">
-                  <v-list-tile avatar :key="item">
-                    <v-list-tile-content>
-                      <v-list-tile-title class="text-xs-center"> {{item}} </v-list-tile-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
-                  <v-divider v-if="index + 1 < hora.length" :key="index"></v-divider>
+        </v-layout> -->
+        <v-navigation-drawer width="220" height="450px" class="mr-3 list-materias" :mini-variant.sync="mini" v-model="drawer" stateless hide-overlay>
+          <v-toolbar fixed flat class="">
+            <v-list class="pa-0">
+              <v-list-tile avatar>
+                <v-list-tile-avatar>
+                  <img src="./../assets/materias.png" >
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>Materias</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-btn icon @click.native.stop="mini = !mini">
+                    <v-icon>chevron_left</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-list>
+          </v-toolbar>
+          <v-list class="scroll-box mt-5" dense>
+            <v-divider></v-divider>
+            <template v-for="materia in sortMaterias">
+              <drag :transfer-data="materia" :key="index">
+                <template slot-scope="props">
+                  <v-card :key="materia.id_materia" dark :color="'color'+ (materia.id_materia+1)">
+                    <v-list-tile avatar  @click="">
+                      <v-list-tile-action>
+                        <i :class="icons[materia.id_materia]"></i>
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{ materia.literal }}</v-list-tile-title>
+                      </v-list-tile-content>
+                      <v-list-tile-action>
+                        <v-btn icon @click.native.stop="">
+                          <v-icon>alert</v-icon>
+                        </v-btn>
+                      </v-list-tile-action>
+                    </v-list-tile>
+                  </v-card>
                 </template>
-              </v-list>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-      <v-flex xs11>
-        <v-layout row>
-          <v-flex xs2 v-for="dia in horario" :key="dia.dia">
-            <v-card dark tile flat color="primary">
-              <v-card-text class="text-xs-center">{{ dia.dia }}</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex xs2 v-for="dia in horario" :key="dia.dia">
-            <v-list class="pt-0 pb-0">
-              <template v-for="(item, index) in dia.periodos">
-                <drop @drop="handleDrop(item, ...arguments)" :key="item.id">
-                  <template slot-scope="props">
-                    <v-card dark :color="props.transferData ? 'color'+(props.transferData.id_materia+1) : (item.id_materia == null ? 'white' : 'color'+ (item.id_materia+1))">
-                      <v-list-tile avatar :id="item.id_materia" @click="">
-                        <drag v-if="item.literal != null" :transfer-data="item" style="height: 100%; width: 100%">
-                          <v-list-tile-content>
-                              <v-list-tile-title>{{ props.transferData ? props.transferData.literal : item.literal }}</v-list-tile-title>
-                          </v-list-tile-content>
-                        </drag>
-                        <v-list-tile-content v-else>
-                          <v-list-tile-title>{{ props.transferData ? props.transferData.literal : item.literal }}</v-list-tile-title>
+              </drag>
+            </template>
+          </v-list>
+        </v-navigation-drawer>
+      <!-- </v-flex>
+      <v-flex xs9> -->
+        <v-layout row wrap>
+          <v-flex xs1>
+            <v-layout row wrap>
+            <v-flex xs12>
+                <v-card tile flat>
+                  <v-card-text class="text-xs-center">HORA</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-card>
+                  <v-list class="pt-0 pb-0">
+                    <template v-for="(item, index) in hora">
+                      <v-list-tile avatar :key="item">
+                        <v-list-tile-content>
+                          <v-list-tile-title class="text-xs-center"> {{item}} </v-list-tile-title>
                         </v-list-tile-content>
                       </v-list-tile>
-                    </v-card>
+                      <v-divider v-if="index + 1 < hora.length" :key="index"></v-divider>
+                    </template>
+                  </v-list>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs11>
+            <v-layout row>
+              <v-flex xs2 v-for="dia in horario" :key="dia.dia">
+                <v-card dark tile flat color="primary">
+                  <v-card-text class="text-xs-center">{{ dia.dia }}</v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs2 v-for="dia in horario" :key="dia.dia">
+                <v-list class="pt-0 pb-0">
+                  <template v-for="(item, index) in dia.periodos">
+                    <drop @drop="handleDrop(item, ...arguments)" :key="item.id">
+                      <template slot-scope="props">
+                        <v-card dark :color="props.transferData ? 'color'+(props.transferData.id_materia+1) : (item.id_materia == null ? 'white' : 'color'+ (item.id_materia+1))">
+                          <v-list-tile avatar :id="item.id_materia" @click="">
+                            <drag v-if="item.literal != null" :transfer-data="item" style="height: 100%; width: 100%">
+                              <v-list-tile-content>
+                                  <v-list-tile-title>{{ props.transferData ? props.transferData.literal : item.literal }}</v-list-tile-title>
+                              </v-list-tile-content>
+                            </drag>
+                            <v-list-tile-content v-else>
+                              <v-list-tile-title>{{ props.transferData ? props.transferData.literal : item.literal }}</v-list-tile-title>
+                            </v-list-tile-content>
+                          </v-list-tile>
+                        </v-card>
+                      </template>
+                    </drop>
+                    <v-divider v-if="index + 1 < dia.periodos.length" :key="index + '-' + item.id"></v-divider>
                   </template>
-                </drop>
-                <v-divider v-if="index + 1 < dia.periodos.length" :key="index + '-' + item.id"></v-divider>
-              </template>
-            </v-list>
+                </v-list>
+              </v-flex>
+            </v-layout>
           </v-flex>
         </v-layout>
-      </v-flex>
+      <!-- </v-flex> -->
     </v-layout>
     <v-snackbar
       :timeout="msg.timeout"
@@ -162,23 +211,46 @@
 
 <script>
 import { mapGetters } from "vuex";
-import store from '@/store/store';
-import resource from '@/util/api-resource';
+import store from "@/store/store";
+import resource from "@/util/api-resource";
 export default {
   data() {
     return {
+      drawer: true,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        mini: true,
+        right: null,
       msg: {
         visible: false,
-        y: 'bottom',
-        x: 'left',
-        mode: '',
+        y: "bottom",
+        x: "left",
+        mode: "",
         timeout: 15000,
-        text: ''
+        text: ""
       },
       curso: null,
       cursoid: null,
       loading: false,
       loading2: false,
+      icons: {
+        6: "fas fa-calculator",
+        2: "fas fa-pencil-alt",
+        4: "fas fa-leaf",
+        14: "fas fa-flask",
+        9: "fas fa-trophy",
+        3: "fas fa-globe",
+        10: "fas fa-music",
+        5: "fas fa-paint-brush",
+        8: "fas fa-language",
+        7: "fas fa-lightbulb",
+        11: "fas fa-diagnoses",
+        1: "fas fa-hands",
+        13: "fas fa-flask",
+        12: "fas fa-book"
+      },
       /* materias: [
         {
           id: 1,
@@ -237,7 +309,7 @@ export default {
       ], */
       materias: [],
       horario: [
-       /*  {
+        /*  {
           dia: "lunes",
           materias: [
             { idPer: 0, id: "", name: "" },
@@ -318,12 +390,12 @@ export default {
       if (!!from) {
         let mat_id = to.id_materia;
         let mat_name = to.literal;
-          to.id_materia = from.id_materia;
-          to.literal = from.literal;
+        to.id_materia = from.id_materia;
+        to.literal = from.literal;
         if (from.id >= 0) {
           from.id_materia = mat_id;
           from.literal = mat_name;
-        } 
+        }
       }
     },
 
@@ -331,22 +403,26 @@ export default {
       this.loading2 = true;
       let creds = {
         materias: {},
-        periodos: {},
-      }
+        periodos: {}
+      };
       for (const key in this.materias) {
         if (this.materias[key].profesor != null) {
-          creds.materias[this.materias[key].id_materia] = this.materias[key].profesor;
+          creds.materias[this.materias[key].id_materia] = this.materias[
+            key
+          ].profesor;
         }
       }
 
       for (const dia in this.horario) {
         for (const periodo in this.horario[dia].periodos) {
-          if(this.horario[dia].periodos[periodo].id_materia != null) {
-            creds.periodos[this.horario[dia].periodos[periodo].id] = this.horario[dia].periodos[periodo].id_materia;
+          if (this.horario[dia].periodos[periodo].id_materia != null) {
+            creds.periodos[
+              this.horario[dia].periodos[periodo].id
+            ] = this.horario[dia].periodos[periodo].id_materia;
           }
         }
       }
-      
+
       //console.log(creds)
 
       resource.horario
@@ -355,7 +431,7 @@ export default {
           this.msg.text = response.msg;
           this.msg.visible = true;
           this.loading2 = false;
-         // console.log(this.cursoid)
+          // console.log(this.cursoid)
         })
         .catch(e => {
           console.log(e);
@@ -364,28 +440,28 @@ export default {
   },
   watch: {
     // whenever question changes, this function will run
-    cursoid: function (newdata, olddata) {
+    cursoid: function(newdata, olddata) {
       if (newdata != null) {
         this.loading = true;
         resource.horario
-        .pedirHorario(newdata)
-        .then(response => {
-          this.materias = response.content.data.materias;
-          this.horario = response.content.data.horario;
-          this.loading = false;
-        })
-        .catch(e => {
-          console.log(e);
-        });
+          .pedirHorario(newdata)
+          .then(response => {
+            this.materias = response.content.data.materias;
+            this.horario = response.content.data.horario;
+            this.loading = false;
+          })
+          .catch(e => {
+            console.log(e);
+          });
       }
     },
 
-    curso: function (newdata, olddata) {
+    curso: function(newdata, olddata) {
       this.cursoid = null;
-    }
+    },
   },
   computed: {
-    ...mapGetters(["getMaterias", 'cursosHabilitados']),
+    ...mapGetters(["getMaterias", "cursosHabilitados"]),
     paralelos() {
       return store.getters.paralelosCurso(this.curso);
     },
@@ -394,13 +470,13 @@ export default {
     },
     lists() {
       let list = [];
-      list.push(this.sortMaterias.slice(0, this.materias.length / 2 + 1));
-      list.push(
+      list.push(this.sortMaterias.slice(0, this.materias.length));
+      /* list.push(
         this.sortMaterias.slice(
           this.materias.length / 2 + 1,
           this.materias.length
         )
-      );
+      ); */
       /* list.push(
         this.sortMaterias.slice(
           this.materias.length / 3 * 2 + 1,
@@ -414,5 +490,18 @@ export default {
 </script>
 
 <style scoped>
+.list-materias {
+  overflow: hidden; 
+}
 
+.scroll-box {
+  overflow-y: auto;
+  height: 400px;
+  padding-top: 15px;
+  overflow-x: hidden;
+  position: absolute;
+  right: -30px;
+  padding-right: 18px;
+  left: 0px;
+}  
 </style>
