4c6439711354995a3f59071f37acb6ef2cdb00e9
Franco Alvarez <wfalvarez1@gmail.com>
2018-06-16 20:49:11 -0400

    Detalles inicio Admin

---
+++ src/views/Reportes.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-jumbotron height="100%" class="content" color="white">
+        <v-container grid-list-sm>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <h1>Reportes</h1>
+                </v-flex>
+                <v-flex d-flex xs12 sm5 md4>
+                    <estado numero="6" icono="./../assets/Kirby.jpg" color="indigo" literal="Docentes"></estado>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-jumbotron>
+</template>
+
+<script>
+import resource from "@/util/api-resource";
+export default {
+  name: "Reportes",
+  components: {
+
+  },
+  mounted() {
+   
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  min-height: 500px;
+}
+</style>
